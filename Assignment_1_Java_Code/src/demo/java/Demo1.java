97a982d4c39eb770a7de051d5aaa0d6634d0472b
Kytan13 <kytanszvs@gmail.com>
2026-02-11 14:47:06 +0000

    Engineer B changes

--- Assignment_1_Java_Code/src/demo/java/Demo1.java
+++ Assignment_1_Java_Code/src/demo/java/Demo1.java
@@ -2,11 +2,21 @@ package demo.java;
 
 public class Demo1 {
 	
-	public int method1(int number) {
-
-		int square = number * number;
-		return square;
-
+	public float method1(float number) {
+		
+			float square = number * number;
+			return square;
+		
 	} // end method1
 
+	public boolean method2(char letter, float number) {
+		
+			if((letter == 'A') && (number == 12.0))
+				return false;
+		
+			return true;
+
+		} // end method2
+
 }// end Demo1
+	
